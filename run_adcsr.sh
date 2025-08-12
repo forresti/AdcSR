74c405d2bb0e2efcf54b8b7fa370d68742eb66e1
forresti <fiandola@gmail.com>
2025-08-12 00:06:54 -0700

    try autocast

--- run_adcsr.sh
+++ run_adcsr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 in_dir=testset/DRealSR/LR
-out_dir=testset/DRealSR/model_output_HR
+out_dir=testset/DRealSR/model_output_HR_fp16
 
 # in_dir=testset/DigitalZoom_SR_dataset_20250307/LR
 # out_dir=testset/DigitalZoom_SR_dataset_20250307/results_fp16
@@ -12,7 +12,10 @@ out_dir=testset/DRealSR/model_output_HR
 mkdir -p $out_dir
 
 #  python test.py \
- python test_fp16_vs_fp32.py \
+# python test_fp16_vs_fp32.py \
+python test_mixed_precision.py \
    --LR_dir=$in_dir \
    --SR_dir=$out_dir \
-   --compare_precision
+   --use_mixed_precision \
+   --precision_mode autocast
+  #  --compare_precision
