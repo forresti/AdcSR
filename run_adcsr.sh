ac5dc1e694b7b93cc3408f99acba2b3998636389
forresti <fiandola@gmail.com>
2025-08-12 00:52:25 -0700

    wip debug fp16

--- run_adcsr.sh
+++ run_adcsr.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-in_dir=testset/DRealSR/LR
-out_dir=testset/DRealSR/model_output_HR_fp16
+# in_dir=testset/DRealSR/LR
+# out_dir=testset/DRealSR/model_output_HR_fp16
 
 # in_dir=testset/DigitalZoom_SR_dataset_20250307/LR
 # out_dir=testset/DigitalZoom_SR_dataset_20250307/results_fp16
 
-# in_dir=testset/sg_cropped/LR_square
-# out_dir=testset/sg_cropped/LR_square_results_fp16
+in_dir=testset/sg_cropped/LR_square
+out_dir=testset/sg_cropped/LR_square_results_fp16
 
 mkdir -p $out_dir
 
