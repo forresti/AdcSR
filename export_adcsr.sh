2bbf23fc5bf10fec05f263822d7e3db9207dc374
forresti <fiandola@gmail.com>
2025-09-10 14:17:58 -0700

    start on model export

---
+++ export_adcsr.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+in_dir=testset/sg_cropped/faces_crop_fi_512
+out_dir=testset/sg_cropped/faces_crop_fi_512_results
+
+mkdir -p $out_dir
+
+ python test.py \
+   --LR_dir=$in_dir \
+   --SR_dir=$out_dir
